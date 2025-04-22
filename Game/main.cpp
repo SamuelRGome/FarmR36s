d3935ab6623c0adae51f04b4fd8d11e4c574a56d
Samuel Rufino <samuelrufino0103@gmail.com>
2025-04-22 15:53:34 -0300

    up

--- Game/main.cpp
+++ Game/main.cpp
@@ -28,8 +28,8 @@ int main(int argc, char *argv[]) {
     // Pega a surface da janela
     screenSurface = SDL_GetWindowSurface(window);
 
-    //Pinta a surface de branco
-    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+    //Pinta a surface de vermelho
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00));
 
     // Atualiza a janela
     SDL_UpdateWindowSurface(window);
