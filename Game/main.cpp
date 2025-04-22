e184708a7369159d41b3a2af9b8dcad896dd5e2d
Samuel Rufino <samuelrufino0103@gmail.com>
2025-04-22 16:00:14 -0300

    change test code

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,53 +1,38 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-int main(int argc, char *argv[]) {
-    
-    // Janela que vai ser renderizada
+int main(int argc, char* argv[])
+{
     SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+    SDL_Rect rect = {295, 215, 50, 50};
 
-    // Surface contido pela janela
-    SDL_Surface* screenSurface = NULL;
-
-    // Inicializa o SDL
-    if( SDL_Init( SDL_INIT_VIDEO) < 0) {
-
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        return 1;
     } else {
-        //Cria a janela
-        window = SDL_CreateWindow( "FarmR36s", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
         if (window == NULL) {
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-            return 1;
-        }
-    } 
+        } else {
+            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+            if (renderer == NULL) {
+                printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+            } else {
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                SDL_RenderClear(renderer);
 
-    // Pega a surface da janela
-    screenSurface = SDL_GetWindowSurface(window);
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+                SDL_RenderFillRect(renderer, &rect);
 
-    //Pinta a surface de vermelho
-    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00));
-
-    // Atualiza a janela
-    SDL_UpdateWindowSurface(window);
-
-    // janela aberta até fechar
-    SDL_Event e;
-    bool quit = false;
-    while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
+                SDL_RenderPresent(renderer);
+                SDL_Delay(2000);
             }
         }
     }
 
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    window = NULL;
-
     SDL_Quit();
+
     return 0;
 }
